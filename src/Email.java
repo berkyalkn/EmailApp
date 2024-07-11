2bada08ab9251c27e6b50d01e938245a9a31e93c
Berkay Alkan <berky.alknn@gmail.com>
2024-07-11 17:30:58 +0300

    define the email department

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
@@ -12,5 +14,24 @@ public class Email {
         this.lastName = lastName;
         System.out.println("Email created --> FirstName : " + firstName +
                 " LastName : " + lastName);
+        this.department = setDepartment();
+        System.out.println("Department : " + this.department);
+    }
+
+    private String setDepartment() {
+
+        System.out.print("Enter the department\n1 for Sales\n2 for Development" +
+                "\n3 for Accounting\n0 for none");
+        Scanner scanner = new Scanner(System.in);
+        int departmentChoice = scanner.nextInt();
+        if (departmentChoice == 1) {
+            return "Sales";
+        } else if (departmentChoice == 2) {
+            return "Development";
+        } else if (departmentChoice == 3) {
+            return "Accounting";
+        } else {
+            return "";
+        }
     }
 }
