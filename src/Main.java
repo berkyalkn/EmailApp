d5a86938240c2f0a0b95af361c365dde20e34911
Berkay Alkan <berky.alknn@gmail.com>
2024-07-15 18:07:43 +0300

    implement class set methods

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         Email berkay = new Email("Berkay", "Alkan");
+        berkay.setMailboxCapacity(400);
+        berkay.changePassword("berkay07");
+        berkay.setAlternateEmail("berky.alknn@gmail.com");
     }
 }
